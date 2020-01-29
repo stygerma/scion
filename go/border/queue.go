ccdc8971e7b8f9e6cdb0cb3c45693995188633ee
Joel Fischer <fischjoe@student.ethz.ch>
2020-01-29 15:15:15 +0100

    Make the dequeuer wait with a channel add a wrapper around rpkts for Marc added function for popping multiple items

--- go/border/queue.go
+++ go/border/queue.go
@@ -6,19 +6,24 @@ import (
 	"github.com/scionproto/scion/go/border/rpkt"
 )
 
+type qPkt struct {
+	queueNo int
+	rp *rpkt.RtrPkt
+}
+
 // Queue is a single queue
 type packetQueue struct {
 	// Id string
 
 	mutex *sync.Mutex
 
-	queue     []*rpkt.RtrPkt
+	queue     []*qPkt
 	length int
 	maxLength int
 	priority  int
 }
 
-func (pq *packetQueue) enqueue(rp *rpkt.RtrPkt) {
+func (pq *packetQueue) enqueue(rp *qPkt) {
 
 	pq.mutex.Lock()
 	defer pq.mutex.Unlock()
@@ -38,12 +43,12 @@ func (pq *packetQueue) getLength() int {
 	return pq.length
 }
 
-func (pq *packetQueue) peek() *rpkt.RtrPkt {
+func (pq *packetQueue) peek() *qPkt {
 
 	return pq.queue[0]
 }
 
-func (pq *packetQueue) pop() *rpkt.RtrPkt {
+func (pq *packetQueue) pop() *qPkt {
 
 	pq.mutex.Lock()
 	defer pq.mutex.Unlock()
@@ -51,6 +56,18 @@ func (pq *packetQueue) pop() *rpkt.RtrPkt {
 	pkt := pq.queue[0]
 	pq.queue = pq.queue[1:]
 	pq.length = pq.length - 1
+
+	return pkt
+}
+
+func (pq *packetQueue) popMultiple(number int) []*qPkt {
+
+	pq.mutex.Lock()
+	defer pq.mutex.Unlock()
+
+	pkt := pq.queue[0:number]
+	pq.queue = pq.queue[number:]
+	pq.length = pq.length - number
 
 	return pkt
 }
