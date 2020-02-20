0cf91039421ac847e4437cb56b89fad45a6d22db
Joel Fischer <fischjoe@netsec-yd3sbg.inf.ethz.ch>
2020-02-20 16:13:50 +0100

    Use the new deficit round robin scheduler
    Rename scheduler from wrr to drr to reflect that it is deficit and not
    weighted round robin

--- go/border/router.go
+++ go/border/router.go
@@ -107,6 +107,9 @@ func (r *Router) Start() {
 		defer log.HandlePanic()
 		rctrl.Control(r.sRevInfoQ, cfg.General.ReconnectToDispatcher)
 	}()
+	go func() {
+		r.dequeuer()
+	}()
 	if err := r.startDiscovery(); err != nil {
 		fatal.Fatal(common.NewBasicError("Unable to start discovery", err))
 	}
