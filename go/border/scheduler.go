305b399ea2bbe61dc62ef90a25c20a8c247cd889
Joel Fischer <fischjoe@netsec-yd3sbg.inf.ethz.ch>
2020-02-21 14:44:10 +0100

    Try to implement the weird thing with minimum and maximum percentages of the bandwidth. Ping works, no other tests though

--- go/border/scheduler.go
+++ go/border/scheduler.go
@@ -33,6 +33,7 @@ func (r *Router) drrDequer() {
 	qsum := 0
 	for i < len(r.queues) {
 		qsum = qsum + r.queues[i].priority
+		i++
 	}
 
 	for {
@@ -40,8 +41,8 @@ func (r *Router) drrDequer() {
 		i := 0
 
 		for i < len(r.queues) {
-			r.drrDequeue((j+i)%(len(r.queues)), 1)
-			i = i + 1
+			r.drrMinMaxDequeue((j+i)%(len(r.queues)), 1)
+			i++
 		}
 	}
 }
@@ -60,6 +61,77 @@ func (r *Router) drrDequeue(queueNo int, qsum int) {
 			r.forwardPacket(qp.rp)
 		}
 	}
+}
+
+func (r *Router) drrMinMaxDequeue(queueNo int, qsum int) {
+
+	length := r.queues[queueNo].getLength()
+	pktToDequeue := min(64*(r.queues[queueNo].minBandwidth/qsum), 1)
+
+	log.Debug("The queue has length", "length", length)
+	log.Debug("Dequeueing packets", "quantum", pktToDequeue)
+
+	if length > 0 {
+
+		if r.surplusAvailable() {
+			log.Debug("Surplus available", "surplus", r.schedulerSurplus)
+			if length > pktToDequeue {
+				pktToDequeue = r.getFromSurplus(queueNo, length)
+				log.Debug("Dequeueing above minimum", "quantum", pktToDequeue)
+			} else {
+				if pktToDequeue-length > 0 {
+					r.payIntoSurplus(queueNo, pktToDequeue-length)
+					log.Debug("Paying into surplus", "payment", pktToDequeue-length)
+				}
+			}
+		}
+
+		qps := r.queues[queueNo].popMultiple(max(length, pktToDequeue))
+		for _, qp := range qps {
+			r.forwardPacket(qp.rp)
+		}
+	}
+}
+
+func (r *Router) getFromSurplus(queueNo int, request int) int {
+
+	r.schedulerSurplusMtx.Lock()
+	defer r.schedulerSurplusMtx.Unlock()
+
+	// Check limit for queue
+	// Take out of surplus
+
+	i := 0
+	qsum := 0
+	for i < len(r.queues) {
+		qsum = qsum + r.queues[i].minBandwidth
+		i++
+	}
+	upperLimit := min(64*(r.queues[queueNo].maxBandwidth/qsum), 1)
+
+	credit := min(r.schedulerSurplus, upperLimit)
+
+	r.schedulerSurplus = r.schedulerSurplus - credit
+
+	return credit
+
+}
+
+func (r *Router) payIntoSurplus(queueNo int, payment int) {
+
+	r.schedulerSurplusMtx.Lock()
+	defer r.schedulerSurplusMtx.Unlock()
+
+	r.schedulerSurplus = r.schedulerSurplus + payment
+
+}
+
+func (r *Router) surplusAvailable() bool {
+
+	r.schedulerSurplusMtx.Lock()
+	defer r.schedulerSurplusMtx.Unlock()
+
+	return r.schedulerSurplus > 0
 }
 
 func max(a, b int) int {
