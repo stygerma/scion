de10c922e4e1caaa4f96a1788089441ac2e8669c
Joel Fischer <fischjoe@netsec-yd3sbg.inf.ethz.ch>
2020-02-27 12:11:28 +0100

    Update

--- go/border/queue_test.go
+++ go/border/queue_test.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"net"
 	"sync"
 	"testing"
 	"time"
+
+	"github.com/scionproto/scion/go/border/brconf"
+	"github.com/scionproto/scion/go/border/rctx"
+	"github.com/scionproto/scion/go/border/rpkt"
+	"github.com/scionproto/scion/go/lib/addr"
+	"github.com/scionproto/scion/go/lib/common"
+	"github.com/scionproto/scion/go/lib/log"
+	"github.com/scionproto/scion/go/lib/ringbuf"
+	"github.com/scionproto/scion/go/lib/spkt"
+	"github.com/scionproto/scion/go/lib/topology"
+	"github.com/scionproto/scion/go/lib/xtest"
 )
 
 /*
@@ -85,3 +97,151 @@ func benchmarkPop(popNo int, b *testing.B) {
 
 func BenchmarkPop1(b *testing.B) { benchmarkPop(1, b) }
 func BenchmarkPop5(b *testing.B) { benchmarkPop(10, b) }
+
+func TestPacketGen(t *testing.T) {
+	_ = jfPrepareRtrPacketSample(t)
+}
+
+func TestThrowPanic(t *testing.T) {
+
+	r, oldCtx := setupTestRouter(t)
+
+}
+
+func jfPrepareRtrPacketSample(t *testing.T) *RtrPkt {
+	r := NewRtrPkt()
+	r.Raw = xtest.MustReadFromFile(t, rawUdpPkt)
+	// Set some other data that are required for the parsing to succeed:
+	var config = &brconf.BRConf{
+		IA: addr.IA{I: 1, A: 2},
+		BR: &topology.BRInfo{
+			IFs: map[common.IFIDType]*topology.IFInfo{5: nil},
+		},
+	}
+	r.Ingress = addrIFPair{IfID: 5}
+	// return r
+
+	r.parseBasic()
+
+	sampleUDPAddr := &net.UDPAddr{IP: net.IPv4(127, 1, 1, 111), Port: -1, Zone: "IPv6 scoped addressing zone"}
+
+	r.Id = "TestPacket000"
+	r.Raw = nil // We don't need contents for this anyways
+	r.TimeIn = time.Now()
+	r.DirFrom = -1 // I don't know what this is
+	r.Ingress = addrIFPair{Dst: sampleUDPAddr,
+		Src:  sampleUDPAddr,
+		IfID: 0, IfLabel: "TODO set all of this stuff correctly"}
+	r.Egress = []EgressPair{EgressPair{S: nil, Dst: sampleUDPAddr}}
+	r.CmnHdr = spkt.CmnHdr{}
+	r.IncrementedPath = false
+	r.idxs = packetIdxs{} // packetIdxs provides offsets into a packet buffer to the start of various, we might actually need Raw
+	r.dstIA = addr.IA{I: 2, A: 10}
+	r.srcIA = addr.IA{I: 1, A: 10}
+	r.dstHost = nil
+	r.srcHost = nil
+	r.infoF = nil
+	r.hopF = nil
+	r.ifCurr = nil
+	r.ifNext = nil
+	r.consDirFlag = nil
+	r.HBHExt = nil
+	r.E2EExt = nil
+	r.L4Type = 1 // L4SCMP
+	r.l4 = nil
+	r.pld = nil
+	r.hooks = hooks{}
+	r.SCMPError = false
+	// r.log.Logger = nil
+	r.Ctx = rctx.New(config)
+	r.refCnt = -1
+	return r
+}
+
+// Copied from setup_test.go:
+
+// setupTest sets up a test router. The test router is initially set up with the
+// topology loaded from testdata.
+func setupTestRouter(t *testing.T) (*Router, *rctx.Ctx) {
+	r := initTestRouter(4)
+	sockConf := brconf.SockConf{Default: PosixSock}
+	// oldCtx contains the testdata topology.
+	oldCtx := rctx.New(loadConfig(t))
+	xtest.FailOnErr(t, r.setupNet(oldCtx, nil, sockConf))
+	startSocks(oldCtx)
+	return r, oldCtx
+}
+
+func initTestRouter(maxNumPosixInput int) *Router {
+	// Init metrics.
+	testInitOnce.Do(func() {
+		// Reduce output displayed in goconvey.
+		log.Root().SetHandler(log.DiscardHandler())
+	})
+	// The number of free packets has to be at least the number of posix
+	// input routines times inputBufCnt. Otherwise they might get stuck
+	// trying to prepare for reading from the connection.
+	// See: https://github.com/scionproto/scion/issues/1981
+	r := &Router{
+		freePkts: ringbuf.New(maxNumPosixInput*inputBufCnt, func() interface{} {
+			return rpkt.NewRtrPkt()
+		}, "free_pkts"),
+	}
+	return r
+}
+
+// updateTestRouter calls setupNet on the provided router with new and old context.
+// The cleanup function shall be called to free the allocated sockets.
+func updateTestRouter(r *Router, newCtx, oldCtx *rctx.Ctx) func() {
+	// Copy the context to make sure all sockets are closed,
+	// even if socket pointers are modified in oldCtx.
+	copyCtx := copyContext(oldCtx)
+	err := r.setupNet(newCtx, oldCtx, brconf.SockConf{Default: PosixSock})
+	SoMsg("err", err, ShouldBeNil)
+	// Close all sockets to allow binding in subsequent tests.
+	cleanUp := func() {
+		closeAllSocks(newCtx)
+		closeAllSocks(oldCtx)
+		closeAllSocks(copyCtx)
+	}
+	return cleanUp
+}
+
+func copyContext(ctx *rctx.Ctx) *rctx.Ctx {
+	c := &rctx.Ctx{}
+	*c = *ctx
+	c.ExtSockIn = make(map[common.IFIDType]*rctx.Sock)
+	c.ExtSockOut = make(map[common.IFIDType]*rctx.Sock)
+	for ifid, sock := range ctx.ExtSockIn {
+		c.ExtSockIn[ifid] = sock
+	}
+	for ifid, sock := range ctx.ExtSockOut {
+		c.ExtSockOut[ifid] = sock
+	}
+	return c
+}
+
+func closeAllSocks(ctx *rctx.Ctx) {
+	if ctx != nil {
+		ctx.LocSockIn.Stop()
+		ctx.LocSockOut.Stop()
+		for ifid := range ctx.ExtSockIn {
+			ctx.ExtSockIn[ifid].Stop()
+			ctx.ExtSockOut[ifid].Stop()
+		}
+	}
+}
+
+func loadConfig(t *testing.T) *brconf.BRConf {
+	topo, err := topology.FromJSONFile("testdata/topology.json")
+	xtest.FailOnErr(t, err)
+	topoBR, ok := topo.BR("br1-ff00_0_111-1")
+	if !ok {
+		t.Fatal("BR ID not found")
+	}
+	return &brconf.BRConf{
+		Topo: topo,
+		IA:   topo.IA(),
+		BR:   &topoBR,
+	}
+}
