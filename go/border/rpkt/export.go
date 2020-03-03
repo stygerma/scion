d64c47ecdd78d8b2245f31d5dfc53a15ebdf7f32
Joel Fischer <fischjoe@netsec-yd3sbg.inf.ethz.ch>
2020-03-03 11:18:31 +0100

    Added a new method for creating mock packets

--- go/border/rpkt/export.go
+++ go/border/rpkt/export.go
@@ -63,3 +63,51 @@ func JFPrepareRtrPacketSample(t *testing.T) *RtrPkt {
 	r.refCnt = -1
 	return r
 }
+
+//TODO: Make this be not dumb, I just want this export for tests. But for some reason it doesn't work.
+func JFPrepareRtrPacketWith(sourceIA addr.IA, destinationIA addr.IA, L4Type common.L4ProtocolType) *RtrPkt {
+	r := NewRtrPkt()
+	// r.Raw = xtest.MustReadFromFile(t, rawUdpPkt)
+	// Set some other data that are required for the parsing to succeed:
+	var config = &brconf.BRConf{
+		IA: addr.IA{I: 1, A: 2},
+		BR: &topology.BRInfo{
+			IFs: map[common.IFIDType]*topology.IFInfo{5: nil},
+		},
+	}
+	r.Ingress = AddrIFPair{IfID: 5}
+
+	sampleUDPAddr := &net.UDPAddr{IP: net.IPv4(127, 1, 1, 111), Port: -1, Zone: "IPv6 scoped addressing zone"}
+
+	r.Id = "TestPacket000"
+	r.Raw = nil // We don't need contents for this anyways
+	r.TimeIn = time.Now()
+	r.DirFrom = -1 // I don't know what this is
+	r.Ingress = AddrIFPair{Dst: sampleUDPAddr,
+		Src:  sampleUDPAddr,
+		IfID: 0, IfLabel: "TODO set all of this stuff correctly"}
+	r.Egress = []EgressPair{EgressPair{S: nil, Dst: sampleUDPAddr}}
+	r.CmnHdr = spkt.CmnHdr{}
+	r.IncrementedPath = false
+	r.idxs = packetIdxs{} // packetIdxs provides offsets into a packet buffer to the start of various, we might actually need Raw
+	r.srcIA = sourceIA
+	r.dstIA = destinationIA
+	r.dstHost = nil
+	r.srcHost = nil
+	r.infoF = nil
+	r.hopF = nil
+	r.ifCurr = nil
+	r.ifNext = nil
+	r.consDirFlag = nil
+	r.HBHExt = nil
+	r.E2EExt = nil
+	r.L4Type = 1 // L4SCMP
+	r.l4 = nil
+	r.pld = nil
+	r.hooks = hooks{}
+	r.SCMPError = false
+	// r.log.Logger = nil
+	r.Ctx = rctx.New(config)
+	r.refCnt = -1
+	return r
+}
