4bbc5f26b7e4aae866bcb780971810debf2e36a1
Joel Fischer <fischjoe@student.ethz.ch>
2020-03-18 12:33:54 +0100

    Added new rules using addr.IA instead of strings. They are not used yet.

--- go/border/classRule.go
+++ go/border/classRule.go
@@ -4,6 +4,8 @@ import (
 	"strings"
 
 	"github.com/scionproto/scion/go/border/rpkt"
+	"github.com/scionproto/scion/go/lib/addr"
+	"github.com/scionproto/scion/go/lib/common"
 	"github.com/scionproto/scion/go/lib/log"
 )
 
@@ -12,12 +14,102 @@ import (
 // Rule contains a rule for matching packets
 type classRule struct {
 	// This is currently means the ID of the sending border router
-	Name          string `yaml:"name"`
-	SourceAs      string `yaml:"sourceAs"`
-	NextHopAs     string `yaml:"nextHopAs"`
-	DestinationAs string `yaml:"DestinationAs"`
-	L4Type        []int  `yaml:"L4Type"`
-	QueueNumber   int    `yaml:"queueNumber"`
+	Name                 string `yaml:"name"`
+	SourceAs             string `yaml:"sourceAs"`
+	SourceMatchMode      int    `yaml:"sourceMatchMode"`
+	NextHopAs            string `yaml:"nextHopAs"`
+	NextHopMatchMode     int    `yaml:"nextHopMatchMode"`
+	DestinationAs        string `yaml:"DestinationAs"`
+	DestinationMatchMode int    `yaml:"destinationMatchMode"`
+	L4Type               []int  `yaml:"L4Type"`
+	QueueNumber          int    `yaml:"queueNumber"`
+}
+
+type internalClassRule struct {
+	// This is currently means the ID of the sending border router
+	Name          string
+	SourceAs      matchRule
+	NextHopAs     matchRule
+	DestinationAs matchRule
+	L4Type        []int
+	QueueNumber   int
+}
+
+type matchRule struct {
+	IA        addr.IA
+	lowLim    addr.IA // Only set if matchMode is Range
+	upLim     addr.IA // Only set if matchMode is Range
+	matchMode matchMode
+}
+
+type matchMode int
+
+const (
+	// EXACT match the exact ISD and AS
+	EXACT matchMode = 0
+	// ISDONLY match the ISD only
+	ISDONLY matchMode = 1
+	// ASONLY match the AS only
+	ASONLY matchMode = 2
+	// RANGE match AS and ISD in this range
+	RANGE matchMode = 3
+	// ANY match anything
+	ANY matchMode = 4
+)
+
+func convClassRuleToInternal(cr classRule) (internalClassRule, error) {
+
+	sourceMatch, err := getMatchFromRule(cr, cr.SourceMatchMode, cr.SourceAs)
+	if err != nil {
+		return internalClassRule{}, err
+	}
+	destinationMatch, err := getMatchFromRule(cr, cr.DestinationMatchMode, cr.DestinationAs)
+	if err != nil {
+		return internalClassRule{}, err
+	}
+	nextHopMatch, err := getMatchFromRule(cr, cr.NextHopMatchMode, cr.NextHopAs)
+	if err != nil {
+		return internalClassRule{}, err
+	}
+
+	return internalClassRule{
+		Name:          cr.Name,
+		SourceAs:      sourceMatch,
+		NextHopAs:     nextHopMatch,
+		DestinationAs: destinationMatch,
+		L4Type:        cr.L4Type,
+		QueueNumber:   cr.QueueNumber}, nil
+}
+
+func getMatchFromRule(cr classRule, matchModeField int, matchRuleField string) (matchRule, error) {
+	switch matchMode(matchModeField) {
+	case EXACT, ASONLY, ISDONLY, ANY:
+		IA, err := addr.IAFromString(matchRuleField)
+		if err != nil {
+			return matchRule{}, err
+		}
+		m := matchRule{IA: IA, lowLim: addr.IA{}, upLim: addr.IA{}, matchMode: matchMode(matchModeField)}
+		return m, nil
+	case RANGE:
+		if matchMode(matchModeField) == RANGE {
+			parts := strings.Split(matchRuleField, "||")
+			if len(parts) != 2 {
+				return matchRule{}, common.NewBasicError("Invalid Class", nil, "raw", matchModeField)
+			}
+			lowLim, err := addr.IAFromString(parts[1])
+			if err != nil {
+				return matchRule{}, err
+			}
+			upLim, err := addr.IAFromString(parts[1])
+			if err != nil {
+				return matchRule{}, err
+			}
+			m := matchRule{IA: addr.IA{}, lowLim: lowLim, upLim: upLim, matchMode: matchMode(matchModeField)}
+			return m, nil
+		}
+	}
+
+	return matchRule{}, common.NewBasicError("Invalid matchMode declared", nil, "matchMode", matchModeField)
 }
 
 func getQueueNumberFor(rp *rpkt.RtrPkt, crs *[]classRule) int {
@@ -30,6 +122,36 @@ func getQueueNumberFor(rp *rpkt.RtrPkt, crs *[]classRule) int {
 		}
 	}
 	return queueNo
+}
+
+func (cr *internalClassRule) matchSingleRule(rp *rpkt.RtrPkt, matchRuleField *matchRule, getIA func() (addr.IA, error)) bool {
+
+	switch matchRuleField.matchMode {
+	case EXACT, ASONLY, ISDONLY, ANY:
+		Addr, err := getIA()
+		if err != nil {
+			return false
+		}
+		return (*matchRuleField).IA.Equal(Addr)
+	case RANGE:
+		addr, err := getIA()
+		if err != nil {
+			return false
+		}
+		if addr.BiggerThan(matchRuleField.lowLim) && addr.SmallerThan(matchRuleField.upLim) {
+			return true
+		}
+	}
+	return false
+}
+
+func (cr *internalClassRule) matchInternalRule(rp *rpkt.RtrPkt) bool {
+
+	sourceMatches := cr.matchSingleRule(rp, &cr.SourceAs, rp.SrcIA)
+	destinationMatches := cr.matchSingleRule(rp, &cr.SourceAs, rp.SrcIA)
+	nextHopMatches := cr.matchSingleRule(rp, &cr.SourceAs, rp.SrcIA)
+
+	return sourceMatches && destinationMatches && nextHopMatches
 }
 
 func (cr *classRule) matchRule(rp *rpkt.RtrPkt) bool {
