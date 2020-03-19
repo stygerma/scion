dc87e6f7f3def3889b3d8875bad916f8da470c39
Joel Fischer <fischjoe@student.ethz.ch>
2020-03-19 21:05:56 +0100

    And queue based on channels, it is the fastest so far

---
+++ go/border/channelQueue.go
@@ -0,0 +1,189 @@
+package main
+
+import (
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/scionproto/scion/go/lib/log"
+)
+
+type channelPacketQueue struct {
+	Name         string          `yaml:"name"`
+	ID           int             `yaml:"id"`
+	MinBandwidth int             `yaml:"CIR"`
+	MaxBandWidth int             `yaml:"PIR"`
+	PoliceRate   int             `yaml:"policeRate"`
+	MaxLength    int             `yaml:"maxLength"`
+	priority     int             `yaml:"priority"`
+	Profile      []actionProfile `yaml:"profile"`
+
+	mutex *sync.Mutex
+
+	queue  chan *qPkt
+	length uint64
+	tb     tokenBucket
+	head   int
+	tail   int
+	mask   int
+}
+
+func (pq *channelPacketQueue) initQueue(mutQue *sync.Mutex, mutTb *sync.Mutex) {
+
+	pq.mutex = mutQue
+	pq.length = 0
+	pq.tb = tokenBucket{
+		MaxBandWidth: pq.PoliceRate,
+		tokens:       pq.PoliceRate,
+		lastRefill:   time.Now(),
+		mutex:        mutTb}
+	pq.queue = make(chan *qPkt, pq.MaxLength)
+	pq.head = 0
+	pq.tail = 0
+	pq.mask = pq.MaxLength - 1
+
+	// fmt.Println("Finish init")
+}
+
+func (pq *channelPacketQueue) enqueue(rp *qPkt) {
+
+	// TODO: Making this lockfree makes it 10 times faster
+	// pq.mutex.Lock()
+	// defer pq.mutex.Unlock()
+
+	// fmt.Println("Enqueue at", pq.tail, "Dequeue at", pq.head)
+	pq.queue <- rp
+
+	atomic.AddUint64(&pq.length, 1)
+	// pq.length = pq.length + 1
+
+}
+
+func (pq *channelPacketQueue) canEnqueue() bool {
+
+	return int(pq.length) < pq.MaxLength
+}
+
+func (pq *channelPacketQueue) canDequeue() bool {
+
+	return pq.head < pq.tail
+}
+
+func (pq *channelPacketQueue) getFillLevel() int {
+
+	return int(pq.length) / int(pq.MaxLength)
+}
+
+func (pq *channelPacketQueue) getLength() int {
+
+	return int(pq.length)
+}
+
+func (pq *channelPacketQueue) peek() *qPkt {
+
+	return nil
+}
+
+func (pq *channelPacketQueue) pop() *qPkt {
+
+	// pq.mutex.Lock()
+	// defer pq.mutex.Unlock()
+
+	// fmt.Println("Enqueue at", pq.tail, "Dequeue at", pq.head)
+
+	// pq.length = pq.length - 1
+
+	c := 1
+	atomic.AddUint64(&pq.length, ^uint64(c-1))
+
+	return <-pq.queue
+}
+
+func (pq *channelPacketQueue) popMultiple(number int) []*qPkt {
+
+	// TODO: Readd this as soon as popMultiple works as standalone
+	// pq.mutex.Lock()
+	// defer pq.mutex.Unlock()
+
+	c := number
+	atomic.AddUint64(&pq.length, ^uint64(c-1))
+	// pq.length = pq.length - number
+
+	pkts := make([]*qPkt, number)
+
+	for i := 0; i < number; i++ {
+		pkts[i] = <-pq.queue
+	}
+
+	return pkts
+}
+
+func (pq *channelPacketQueue) checkAction() policeAction {
+
+	level := pq.getFillLevel()
+
+	log.Info("Current level is", "level", level)
+	log.Info("Profiles are", "profiles", pq.Profile)
+
+	for j := len(pq.Profile) - 1; j >= 0; j-- {
+		if level >= pq.Profile[j].FillLevel {
+			log.Info("Matched a rule!")
+			if rand.Intn(100) < (pq.Profile[j].Prob) {
+				log.Info("Take Action!")
+				return pq.Profile[j].Action
+			}
+			log.Info("Do not take Action")
+
+		}
+	}
+
+	return PASS
+}
+
+func (pq *channelPacketQueue) police(qp *qPkt, shouldLog bool) policeAction {
+	pq.tb.mutex.Lock()
+	defer pq.tb.mutex.Unlock()
+
+	packetSize := (qp.rp.Bytes().Len()) // In byte
+
+	tokenForPacket := packetSize * 8 // In bit
+
+	if shouldLog {
+		log.Debug("Overall available bandwidth per second", "MaxBandWidth", pq.tb.MaxBandWidth)
+		log.Debug("Spent token in last period", "#tokens", pq.tb.tokenSpent)
+		log.Debug("Available bandwidth before refill", "bandwidth", pq.tb.tokens)
+	}
+
+	pq.tb.refill(shouldLog)
+
+	if shouldLog {
+		log.Debug("Available bandwidth after refill", "bandwidth", pq.tb.tokens)
+		log.Debug("Tokens necessary for packet", "tokens", tokenForPacket)
+		log.Debug("Tokens necessary for packet", "bytes", qp.rp.Bytes().Len())
+	}
+
+	if pq.tb.tokens-tokenForPacket > 0 {
+		pq.tb.tokens = pq.tb.tokens - tokenForPacket
+		pq.tb.tokenSpent += tokenForPacket
+		qp.act.action = PASS
+		qp.act.reason = None
+	} else {
+		qp.act.action = DROP
+		qp.act.reason = BandWidthExceeded
+	}
+
+	if shouldLog {
+		log.Debug("Available bandwidth after update", "bandwidth", pq.tb.tokens)
+	}
+
+	return qp.act.action
+}
+
+func (pq *channelPacketQueue) getMinBandwidth() int {
+	return pq.MinBandwidth
+}
+
+func (pq *channelPacketQueue) getPriority() int {
+	return pq.priority
+}
