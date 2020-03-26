c237f0e90d060df299da596098a57190c510d67e
Joel Fischer <fischjoe@netsec-yd3sbg.inf.ethz.ch>
2020-03-26 16:02:49 +0100

    Removed tests for now

--- go/border/qosqueues/bufQueue_test.go
+++ go/border/qosqueues/bufQueue_test.go
@@ -15,52 +15,46 @@
 
 package qosqueues
 
-import (
-	"sync"
-	"testing"
-	"time"
-)
+// func setupBufQueue() packetBufQueue {
 
-func setupBufQueue() packetBufQueue {
+// 	bandwidth := 0
+// 	priority := 1
 
-	bandwidth := 0
-	priority := 1
+// 	bucket := tokenBucket{MaxBandWidth: bandwidth, tokens: bandwidth, lastRefill: time.Now(), mutex: &sync.Mutex{}}
+// 	que := packetBufQueue{MaxLength: 128, MinBandwidth: priority, MaxBandWidth: priority, mutex: &sync.Mutex{}, tb: bucket}
 
-	bucket := tokenBucket{MaxBandWidth: bandwidth, tokens: bandwidth, lastRefill: time.Now(), mutex: &sync.Mutex{}}
-	que := packetBufQueue{MaxLength: 128, MinBandwidth: priority, MaxBandWidth: priority, mutex: &sync.Mutex{}, tb: bucket}
+// 	que.InitQueue(&sync.Mutex{}, &sync.Mutex{})
 
-	que.InitQueue(&sync.Mutex{}, &sync.Mutex{})
+// 	return que
 
-	return que
+// }
 
-}
+// func TestBasicEnqueueBuf(t *testing.T) {
+// 	que := setupBufQueue()
+// 	pkt := setupQueuePaket()
+// 	que.Enqueue(&pkt)
+// 	length := que.GetLength()
+// 	if length != 1 {
+// 		t.Errorf("Enqueue one packet should give length 1 gave length %d", length)
+// 	}
+// 	pktb := que.Pop()
 
-func TestBasicEnqueueBuf(t *testing.T) {
-	que := setupBufQueue()
-	pkt := setupQueuePaket()
-	que.Enqueue(&pkt)
-	length := que.GetLength()
-	if length != 1 {
-		t.Errorf("Enqueue one packet should give length 1 gave length %d", length)
-	}
-	pktb := que.Pop()
+// 	if &pkt != pktb {
+// 		t.Errorf("Returned wrong packet")
+// 	}
+// }
 
-	if &pkt != pktb {
-		t.Errorf("Returned wrong packet")
-	}
-}
+// func TestBasicEnqueueBufMultidequeue(t *testing.T) {
+// 	que := setupBufQueue()
+// 	pkt := setupQueuePaket()
+// 	que.Enqueue(&pkt)
+// 	length := que.GetLength()
+// 	if length != 1 {
+// 		t.Errorf("Enqueue one packet should give length 1 gave length %d", length)
+// 	}
+// 	pktb := que.PopMultiple(1)[0]
 
-func TestBasicEnqueueBufMultidequeue(t *testing.T) {
-	que := setupBufQueue()
-	pkt := setupQueuePaket()
-	que.Enqueue(&pkt)
-	length := que.GetLength()
-	if length != 1 {
-		t.Errorf("Enqueue one packet should give length 1 gave length %d", length)
-	}
-	pktb := que.PopMultiple(1)[0]
-
-	if &pkt != pktb {
-		t.Errorf("Returned wrong packet")
-	}
-}
+// 	if &pkt != pktb {
+// 		t.Errorf("Returned wrong packet")
+// 	}
+// }
