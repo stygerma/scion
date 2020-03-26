c31c90fd17b7ec03576590748c00c82fc0874bd1
Joel Fischer <fischjoe@netsec-yd3sbg.inf.ethz.ch>
2020-03-26 16:02:49 +0100

    Removed tests for now

--- go/border/qosqueues/policer_test.go
+++ go/border/qosqueues/policer_test.go
@@ -1,5 +1,5 @@
 // Copyright 2020 ETH Zurich
-// Copyright 2018 ETH Zurich, Anapaya Systems
+// Copyright 2020 ETH Zurich, Anapaya Systems
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
+package qosqueues
 
 // import (
 // 	"fmt"
