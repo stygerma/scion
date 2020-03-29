ba172b106ceaa03594e3cd831cc7d33c04eaac2b
Joel Fischer <fischjoe@student.ethz.ch>
2020-03-29 16:32:06 +0200

    Intermediary commit bc things work and they used not to

--- go/border/qos/qosConfig_test.go
+++ go/border/qos/qosConfig_test.go
@@ -27,6 +27,5 @@ func TestForMarc(t *testing.T) {
 	fmt.Println("CongWarning is", qosConfig.config.Queues[1].GetPacketQueue().CongWarning)
 	fmt.Println("CongWarning is", qosConfig.config.Queues[2].GetPacketQueue().CongWarning)
 
-	t.Errorf("FUCK")
-	t.Errorf("IntMin(2, -2) = %d; want -2", 17)
+	// t.Errorf("Show Logs")
 }
