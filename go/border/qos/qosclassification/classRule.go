0efba66442cb4951be9c9e65d0e7917c2edc0e28
Joel Fischer <fischjoe@student.ethz.ch>
2020-04-01 14:54:26 +0200

    Start ClassificationInterface

---
+++ go/border/qos/qosclassification/classRule.go
@@ -0,0 +1,246 @@
+// Copyright 2020 ETH Zurich
+// Copyright 2020 ETH Zurich, Anapaya Systems
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package qosclassification
+
+import (
+	"strings"
+
+	"github.com/scionproto/scion/go/border/qos/qosconf"
+
+	"github.com/scionproto/scion/go/border/rpkt"
+	"github.com/scionproto/scion/go/lib/addr"
+	"github.com/scionproto/scion/go/lib/common"
+)
+
+// TODO: Matching rules is currently based on string comparisons
+
+type InternalClassRule struct {
+	// This is currently means the ID of the sending border router
+	Name          string
+	Priority      int
+	SourceAs      matchRule
+	NextHopAs     matchRule
+	DestinationAs matchRule
+	L4Type        []common.L4ProtocolType
+	QueueNumber   int
+}
+
+type matchRule struct {
+	IA        addr.IA
+	lowLim    addr.IA // Only set if matchMode is Range
+	upLim     addr.IA // Only set if matchMode is Range
+	matchMode matchMode
+}
+
+type matchMode int
+
+const (
+	// EXACT match the exact ISD and AS
+	EXACT matchMode = 0
+	// ISDONLY match the ISD only
+	ISDONLY matchMode = 1
+	// ASONLY match the AS only
+	ASONLY matchMode = 2
+	// RANGE match AS and ISD in this range
+	RANGE matchMode = 3
+	// ANY match anything
+	ANY matchMode = 4
+)
+
+type ClassifierInterface interface {
+	func GetQueueNumber(config *InternalRouterConfig, rp *rpkt.RtrPkt) int
+
+}
+
+func ConvClassRuleToInternal(cr qosconf.ExternalClassRule) (InternalClassRule, error) {
+
+	sourceMatch, err := getMatchFromRule(cr, cr.SourceMatchMode, cr.SourceAs)
+	if err != nil {
+		return InternalClassRule{}, err
+	}
+	destinationMatch, err := getMatchFromRule(cr, cr.DestinationMatchMode, cr.DestinationAs)
+	if err != nil {
+		return InternalClassRule{}, err
+	}
+
+	l4t := make([]common.L4ProtocolType, 0)
+
+	for _, l4pt := range cr.L4Type {
+		l4t = append(l4t, common.L4ProtocolType(l4pt))
+
+	}
+
+	rule := InternalClassRule{
+		Name:          cr.Name,
+		Priority:      cr.Priority,
+		SourceAs:      sourceMatch,
+		DestinationAs: destinationMatch,
+		L4Type:        l4t,
+		QueueNumber:   cr.QueueNumber}
+
+	return rule, nil
+}
+
+func RulesToMap(crs []InternalClassRule) (map[addr.IA][]*InternalClassRule, map[addr.IA][]*InternalClassRule) {
+	sourceRules := make(map[addr.IA][]*InternalClassRule)
+	destinationRules := make(map[addr.IA][]*InternalClassRule)
+
+	for i, cr := range crs {
+		if cr.SourceAs.matchMode == RANGE {
+			lowLimI := uint16(cr.SourceAs.lowLim.I)
+			upLimI := uint16(cr.SourceAs.upLim.I)
+			lowLimA := uint64(cr.SourceAs.lowLim.A)
+			upLimA := uint64(cr.SourceAs.upLim.A)
+
+			for i := lowLimI; i <= upLimI; i++ {
+				for j := lowLimA; j <= upLimA; j++ {
+					sourceRules[addr.IA{I: addr.ISD(i), A: addr.AS(j)}] = append(sourceRules[addr.IA{I: addr.ISD(i), A: addr.AS(j)}], &crs[i])
+				}
+			}
+
+		} else {
+
+			sourceRules[cr.SourceAs.IA] = append(sourceRules[cr.SourceAs.IA], &crs[i])
+		}
+		if cr.DestinationAs.matchMode == RANGE {
+			lowLimI := uint16(cr.DestinationAs.lowLim.I)
+			upLimI := uint16(cr.DestinationAs.upLim.I)
+			lowLimA := uint64(cr.DestinationAs.lowLim.A)
+			upLimA := uint64(cr.DestinationAs.upLim.A)
+
+			for i := lowLimI; i <= upLimI; i++ {
+				for j := lowLimA; j <= upLimA; j++ {
+					addr := addr.IA{I: addr.ISD(i), A: addr.AS(j)}
+					destinationRules[addr] = append(destinationRules[addr], &crs[i])
+				}
+			}
+		} else {
+			destinationRules[cr.DestinationAs.IA] = append(destinationRules[cr.DestinationAs.IA], &crs[i])
+		}
+	}
+
+	return sourceRules, destinationRules
+
+}
+
+
+
+var matches = make([]InternalClassRule, 0)
+var returnRule InternalClassRule
+
+func GetRuleWithHashFor(config *InternalRouterConfig, rp *rpkt.RtrPkt) *InternalClassRule {
+
+	srcAddr, _ := rp.SrcIA()
+	dstAddr, _ := rp.DstIA()
+
+	queues1 := config.SourceRules[srcAddr]
+	queues2 := config.DestinationRules[dstAddr]
+
+	matches = []InternalClassRule{}
+	returnRule = InternalClassRule{QueueNumber: 0}
+
+	for _, rul1 := range queues1 {
+		for _, rul2 := range queues2 {
+			if rul1 == rul2 {
+				matches = append(matches, *rul1)
+			}
+		}
+	}
+
+	max := -1
+	for _, rul1 := range matches {
+		if rul1.Priority > max {
+			returnRule = rul1
+			max = rul1.Priority
+		}
+	}
+
+	return &returnRule
+}
+
+func GetQueueNumberWithHashFor(config *InternalRouterConfig, rp *rpkt.RtrPkt) int {
+
+	return GetRuleWithHashFor(config, rp).QueueNumber
+}
+
+
+
+func (cr *InternalClassRule) matchSingleRule(rp *rpkt.RtrPkt, matchRuleField *matchRule, getIA func() (addr.IA, error)) bool {
+
+	switch matchRuleField.matchMode {
+	case EXACT, ASONLY, ISDONLY, ANY:
+		Addr, err := getIA()
+		if err != nil {
+			return false
+		}
+		return (*matchRuleField).IA.Equal(Addr)
+	case RANGE:
+		addr, err := getIA()
+		if err != nil {
+			return false
+		}
+		if addr.BiggerThan(matchRuleField.lowLim) && addr.SmallerThan(matchRuleField.upLim) {
+			return true
+		}
+	}
+	return false
+}
+
+// func (cr *InternalClassRule) matchInternalRule(rp *rpkt.RtrPkt) bool {
+
+// 	sourceMatches := cr.matchSingleRule(rp, &cr.SourceAs, rp.SrcIA)
+// 	destinationMatches := cr.matchSingleRule(rp, &cr.DestinationAs, rp.DstIA)
+
+// 	return sourceMatches && destinationMatches
+// }
+
+// func matchRuleFromConfig(cr *qosconf.ExternalClassRule, rp *rpkt.RtrPkt) bool {
+
+// 	match := true
+
+// 	srcAddr, _ := rp.SrcIA()
+// 	// log.Debug("Source Address is " + srcAddr.String())
+// 	// log.Debug("Comparing " + srcAddr.String() + " and " + cr.SourceAs)
+// 	if !strings.Contains(srcAddr.String(), cr.SourceAs) {
+// 		match = false
+// 	}
+
+// 	dstAddr, _ := rp.DstIA()
+// 	// log.Debug("Destination Address is " + dstAddr.String())
+// 	// log.Debug("Comparing " + dstAddr.String() + " and " + cr.DestinationAs)
+// 	if !strings.Contains(dstAddr.String(), cr.DestinationAs) {
+// 		match = false
+// 	}
+
+// 	// log.Debug("L4Type is", "L4Type", rp.CmnHdr.NextHdr)
+// 	// log.Debug("L4Type as int is", "L4TypeInt", int(rp.CmnHdr.NextHdr))
+// 	if !contains(cr.L4Type, int(rp.CmnHdr.NextHdr)) {
+// 		match = false
+// 	} else {
+// 		// log.Debug("Matched an L4Type!")
+// 	}
+
+// 	return match
+// }
+
+// func contains(slice []int, term int) bool {
+// 	for _, item := range slice {
+// 		if item == term {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
