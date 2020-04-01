e1ad86086da46202b66b755b8fde9118737f1aa2
Joel Fischer <fischjoe@student.ethz.ch>
2020-04-01 18:40:58 +0200

    Can now match everything. Pretty slow though around 7ms

--- go/border/qos/qosqueues/classRule_test.go
+++ go/border/qos/qosqueues/classRule_test.go
@@ -40,11 +40,104 @@ func TestRulesWithPriority(t *testing.T) {
 		qosConfig, _ := qos.InitQos(extConf, forwardPacketByDrop)
 		pkt := rpkt.PrepareRtrPacketWithStrings(tab.srcIA, tab.dstIA, 1)
 
-		queueNo := qosqueues.GetQueueNumberWithHashFor(qosConfig.GetConfig(), pkt)
+		queueNo := qosqueues.GetQueueNumberForPacket(qosConfig.GetConfig(), pkt)
 		if queueNo != tab.goldenQueueNo {
 			fmt.Println(tab.srcIA, tab.dstIA)
 			t.Errorf("%d Queue number should be %d but is %d", k, tab.goldenQueueNo, queueNo)
 		}
+	}
+
+}
+
+func BenchmarkRuleMatchModes(b *testing.B) {
+	extConf, _ := qosconf.LoadConfig("../testdata/matchTypeTest-config.yaml")
+	qosConfig, _ := qos.InitQos(extConf, forwardPacketByDrop)
+
+	tables := []struct {
+		srcIA       string
+		dstIA       string
+		ruleName    string
+		queueNumber int
+		shouldMatch bool
+	}{
+		{"11-ff00:0:299", "22-ff00:0:188", "Exact - Exact", 1, true},
+		{"33-ff00:0:277", "44-ff00:0:166", "Exact - ISDONLY", 2, true},
+		{"33-ff00:0:277", "44-ff00:0:165", "Exact - ISDONLY", 2, true},
+		{"33-ff00:0:277", "44-ff00:0:000", "Exact - ISDONLY", 2, true},
+		{"55-ff00:0:055", "66-ff00:0:344", "Exact - ASONLY", 3, true},
+		{"55-ff00:0:055", "12-ff00:0:344", "Exact - ASONLY", 3, true},
+		{"55-ff00:0:055", "13-ff00:0:344", "Exact - ASONLY", 3, true},
+		{"55-ff00:0:055", "14-ff00:0:344", "Exact - ASONLY", 3, true},
+		{"77-ff00:0:233", "85-ff00:0:222", "Exact - RANGE", 4, true},
+		{"77-ff00:0:233", "89-ff00:0:222", "Exact - RANGE", 4, true},
+		{"2-ff00:0:011", "89-ff00:0:222", "Exact - RANGE", 4, false},
+		{"2-ff00:0:011", "89-ff00:0:222", "Exact - ANY", 5, true},
+		{"2-ff00:0:011", "89-ff00:0:344", "Exact - ANY", 5, true},
+		{"2-ff00:0:011", "344-ff00:0:222", "Exact - ANY", 5, true},
+		{"2-ff00:0:011", "22-344:0:222", "Exact - ANY", 5, true},
+		{"2-ff00:0:011", "123-ff00:344:222", "Exact - ANY", 5, true},
+	}
+
+	arr := make([]rpkt.RtrPkt, len(tables))
+
+	for k, tab := range tables {
+		arr[k] = *rpkt.PrepareRtrPacketWithStrings(tab.srcIA, tab.dstIA, 1)
+	}
+
+	for i := 0; i < b.N; i++ {
+		for k := 0; k < len(tables); k++ {
+			rul := qosqueues.GetRuleForPacket(qosConfig.GetConfig(), &arr[k])
+			_ = rul
+
+		}
+	}
+
+}
+
+func TestRuleMatchModes(t *testing.T) {
+
+	extConf, _ := qosconf.LoadConfig("../testdata/matchTypeTest-config.yaml")
+	qosConfig, _ := qos.InitQos(extConf, forwardPacketByDrop)
+
+	tables := []struct {
+		srcIA       string
+		dstIA       string
+		ruleName    string
+		queueNumber int
+		shouldMatch bool
+	}{
+		{"11-ff00:0:299", "22-ff00:0:188", "Exact - Exact", 1, true},
+		{"33-ff00:0:277", "44-ff00:0:166", "Exact - ISDONLY", 2, true},
+		{"33-ff00:0:277", "44-ff00:0:165", "Exact - ISDONLY", 2, true},
+		{"33-ff00:0:277", "44-ff00:0:000", "Exact - ISDONLY", 2, true},
+		{"55-ff00:0:055", "66-ff00:0:344", "Exact - ASONLY", 3, true},
+		{"55-ff00:0:055", "12-ff00:0:344", "Exact - ASONLY", 3, true},
+		{"55-ff00:0:055", "13-ff00:0:344", "Exact - ASONLY", 3, true},
+		{"55-ff00:0:055", "14-ff00:0:344", "Exact - ASONLY", 3, true},
+		{"77-ff00:0:233", "85-ff00:0:222", "Exact - RANGE", 4, true},
+		{"77-ff00:0:233", "89-ff00:0:222", "Exact - RANGE", 4, true},
+		{"2-ff00:0:011", "89-ff00:0:222", "Exact - RANGE", 4, false},
+		{"2-ff00:0:011", "89-ff00:0:222", "Exact - ANY", 5, true},
+		{"2-ff00:0:011", "89-ff00:0:344", "Exact - ANY", 5, true},
+		{"2-ff00:0:011", "344-ff00:0:222", "Exact - ANY", 5, true},
+		{"2-ff00:0:011", "22-344:0:222", "Exact - ANY", 5, true},
+		{"2-ff00:0:011", "123-ff00:344:222", "Exact - ANY", 5, true},
+	}
+
+	for k, tab := range tables {
+		pkt := rpkt.PrepareRtrPacketWithStrings(tab.srcIA, tab.dstIA, 1)
+
+		rul := qosqueues.GetRuleForPacket(qosConfig.GetConfig(), pkt)
+		queue := qosqueues.GetQueueNumberForPacket(qosConfig.GetConfig(), pkt)
+
+		if (rul.Name == tab.ruleName) != tab.shouldMatch {
+			t.Errorf("%d should match rule %v but matches rule %v", k, tab.ruleName, rul.Name)
+		}
+
+		if (queue == tab.queueNumber) != tab.shouldMatch {
+			t.Errorf("%d should match queue %v but matches queue %v", k, tab.queueNumber, queue)
+		}
+
 	}
 
 }
