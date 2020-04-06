ad336bc347a8dee23a20066eff713883efa6d8e5
Juan A. Garcia Pardo <juagargi@gmail.com>
2020-04-06 18:31:28 +0200

    fix UT/benchmark

--- go/border/qos/qos_test.go
+++ go/border/qos/qos_test.go
@@ -1,78 +1,27 @@
 package qos
 
 import (
-	"fmt"
 	"io/ioutil"
-	"log"
-	"os"
-	"runtime/pprof"
 	"testing"
 
 	"github.com/inconshreveable/log15"
+	"github.com/stretchr/testify/require"
+
+	"github.com/scionproto/scion/go/border/qos/qosconf"
 	"github.com/scionproto/scion/go/border/rpkt"
 )
 
-const configFileLocation = "/home/fischjoe/go/src/github.com/joelfischerr/scion/go/border/qos/sample-config.yaml"
-
-func TestForMarc(t *testing.T) {
-
-	fmt.Println("Hello test 2")
-
-	qosConfig, _ := InitQos(configFileLocation, nil)
-
-	fmt.Println("Config is", qosConfig)
-
-	fmt.Println("Name is", qosConfig.config.Queues[0].GetPacketQueue().Name)
-	fmt.Println("Name is", qosConfig.config.Queues[1].GetPacketQueue().Name)
-	fmt.Println("Name is", qosConfig.config.Queues[2].GetPacketQueue().Name)
-
-	fmt.Println("Profile is", qosConfig.config.Queues[0].GetPacketQueue().Profile)
-	fmt.Println("Profile is", qosConfig.config.Queues[1].GetPacketQueue().Profile)
-	fmt.Println("Profile is", qosConfig.config.Queues[2].GetPacketQueue().Profile)
-
-	fmt.Println("CongWarning is", qosConfig.config.Queues[0].GetPacketQueue().CongWarning)
-	fmt.Println("CongWarning is", qosConfig.config.Queues[1].GetPacketQueue().CongWarning)
-	fmt.Println("CongWarning is", qosConfig.config.Queues[2].GetPacketQueue().CongWarning)
-
-	// t.Errorf("Show Logs")
-}
-
-func getPackets(numberOfPackets int) []*rpkt.RtrPkt {
-
-	pkt := rpkt.PrepareRtrPacketWithStrings("1-ff00:0:110", "1-ff00:0:111", 1)
-	pkt1 := rpkt.PrepareRtrPacketWithStrings("2-ff00:0:212", "1-ff00:0:111", 1)
-	pkt2 := rpkt.PrepareRtrPacketWithStrings("3-ff00:0:212", "1-ff00:0:111", 1)
-	pkt3 := rpkt.PrepareRtrPacketWithStrings("4-ff00:0:212", "1-ff00:0:111", 1)
-	pkt4 := rpkt.PrepareRtrPacketWithStrings("5-ff00:0:212", "1-ff00:0:111", 1)
-	pkt5 := rpkt.PrepareRtrPacketWithStrings("6-ff00:0:212", "1-ff00:0:111", 1)
-
-	arr := make([]*rpkt.RtrPkt, numberOfPackets)
-
-	for i := 0; i < numberOfPackets-5; i++ {
-		arr[i+0] = pkt
-		arr[i+1] = pkt1
-		arr[i+2] = pkt2
-		arr[i+3] = pkt3
-		arr[i+4] = pkt4
-		arr[i+5] = pkt5
-	}
-
-	return arr
-}
-
 var blocks chan bool
 
-func BenchmarkQueueSinglePacket(b *testing.B) {
-
+func bBenchmarkQueueSinglePacket(b *testing.B) {
 	root := log15.Root()
-
 	file, err := ioutil.TempFile("", "benchmark-log")
-	if err != nil {
-		b.Fatalf("Unexpected error: %v", err)
-	}
+	require.NoError(b, err)
 	root.SetHandler(log15.Must.FileHandler(file.Name(), log15.LogfmtFormat()))
 
-	qosConfig, _ := InitQos(configFileLocation, forwardPacketByDrop)
+	extConfig, err := qosconf.LoadConfig("testdata/sample-config.yaml")
+	require.NoError(b, err)
+	qosConfig, _ := InitQos(extConfig, forwardPacketByDrop)
 	singlePkt := rpkt.PrepareRtrPacketWithStrings("1-ff00:0:110", "1-ff00:0:111", 1)
 
 	b.ResetTimer()
@@ -81,32 +30,44 @@ func BenchmarkQueueSinglePacket(b *testing.B) {
 	}
 }
 
+// BenchmarkQueueSinglePacket measures the performance of the queue. Run with
+// go test -v -run=^$ -bench=BenchmarkQueueSinglePacket ./go/border/qos/ \
+//    -benchtime=20s -cpuprofile=newprofile.pprof
+func BenchmarkQueueSinglePacket(t *testing.B) {
+	root := log15.Root()
+	file, err := ioutil.TempFile("", "benchmark-log")
+	require.NoError(t, err)
+	root.SetHandler(log15.Must.FileHandler(file.Name(), log15.LogfmtFormat()))
+
+	extConfig, err := qosconf.LoadConfig("testdata/sample-config.yaml")
+	require.NoError(t, err)
+	qosConfig, _ := InitQos(extConfig, forwardPacketByDrop)
+	arr := getPackets(1)
+
+	t.ResetTimer()
+	for n := 0; n < t.N; n++ {
+		for _, pkt := range arr {
+			qosConfig.QueuePacket(pkt)
+		}
+	}
+}
+
 func forwardPacketByDrop(rp *rpkt.RtrPkt) {
 	rp.Release()
 }
 
-func TestQueueSinglePacket(t *testing.T) {
-
-	root := log15.Root()
-
-	file, err := ioutil.TempFile("", "benchmark-log")
-	if err != nil {
-		t.Fatalf("Unexpected error: %v", err)
+func getPackets(numberOfPackets int) []*rpkt.RtrPkt {
+	pkts := []*rpkt.RtrPkt{
+		rpkt.PrepareRtrPacketWithStrings("1-ff00:0:110", "1-ff00:0:111", 1),
+		rpkt.PrepareRtrPacketWithStrings("2-ff00:0:212", "1-ff00:0:111", 1),
+		rpkt.PrepareRtrPacketWithStrings("3-ff00:0:212", "1-ff00:0:111", 1),
+		rpkt.PrepareRtrPacketWithStrings("4-ff00:0:212", "1-ff00:0:111", 1),
+		rpkt.PrepareRtrPacketWithStrings("5-ff00:0:212", "1-ff00:0:111", 1),
+		rpkt.PrepareRtrPacketWithStrings("6-ff00:0:212", "1-ff00:0:111", 1),
 	}
-	root.SetHandler(log15.Must.FileHandler(file.Name(), log15.LogfmtFormat()))
-
-	qosConfig, _ := InitQos(configFileLocation, forwardPacketByDrop)
-	arr := getPackets(7 * 5 * 1000 * 1000) // To get to around 20 seconds on my machine which will give a decent profile
-
-	f, err := os.Create("sadJoelProfile.prof")
-	if err != nil {
-		log.Fatal(err)
+	arr := make([]*rpkt.RtrPkt, numberOfPackets*len(pkts))
+	for i := 0; i < numberOfPackets; i++ {
+		copy(arr[i*len(pkts):], pkts)
 	}
-	pprof.StartCPUProfile(f)
-	defer pprof.StopCPUProfile()
-
-	for _, pkt := range arr {
-		qosConfig.QueuePacket(pkt)
-	}
-
+	return arr
 }
