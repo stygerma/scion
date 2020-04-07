073923d1d816d4ae65b008f1171c02beb376b88f
Juan A. Garcia Pardo <juagargi@gmail.com>
2020-04-07 14:38:45 +0200

    Remove duplicate PoliceAction
    # Conflicts:
    #	go/border/qos/queues/bufQueue.go
    #	go/border/qos/queues/channelQueue.go
    #	go/border/qos/queues/policer.go
    #	go/border/router.go

--- go/border/router.go
+++ go/border/router.go
@@ -212,14 +212,8 @@ func (r *Router) forwardPacket(rp *rpkt.RtrPkt) {
 	r.qosConfig.QueuePacket(rp)
 }
 
-// var ticker = time.NewTicker(400 * time.Microsecond)
-
 func (r *Router) forwardPacket(rp *rpkt.RtrPkt) {
 	defer rp.Release()
-
-	// if r.Id == "br1-ff00_0_110-1" {
-	// 	<-ticker.C
-	// }
 
 	// Forward the packet. Packets destined to self are forwarded to the local dispatcher.
 	if err := rp.Route(); err != nil {
