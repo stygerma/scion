5336d724fdb647b3c6f68ed11d0fbef9f3f015bc
Joel Fischer <fischjoe@student.ethz.ch>
2020-04-08 19:37:29 +0200

    UP

--- go/border/qos/scheduler/drrScheduler.go
+++ go/border/qos/scheduler/drrScheduler.go
@@ -10,13 +10,13 @@ import (
 
 // This is a deficit round robin dequeuer. Queues with higher priority will have more packets dequeued at the same time.
 
-type deficitRoundRobinScheduler struct {
+type DeficitRoundRobinScheduler struct {
 	quantumSum  int
 	totalLength int
 	messages    chan bool
 }
 
-var _ SchedulerInterface = (*deficitRoundRobinScheduler)(nil)
+var _ SchedulerInterface = (*DeficitRoundRobinScheduler)(nil)
 
 func (sched *deficitRoundRobinScheduler) Init(routerConfig queues.InternalRouterConfig) {
 
@@ -29,21 +29,33 @@ func (sched *deficitRoundRobinScheduler) Init(routerConfig queues.InternalRouter
 
 }
 
-func (sched *deficitRoundRobinScheduler) Dequeue(queue qosqueues.PacketQueueInterface, forwarder func(rp *rpkt.RtrPkt), queueNo int) {
+func getNoPacketsToDequeue(totalLength int, priority int, totalPriority int) int {
+	return int(100.0 / float64(totalPriority) * float64(priority))
+}
 
-	length := queue.GetLength()
-	var nopkts int = 64 * (queue.GetPriority() / sched.quantumSum)
+func (sched *DeficitRoundRobinScheduler) Dequeue(queue qosqueues.PacketQueueInterface, forwarder func(rp *rpkt.RtrPkt), queueNo int) {
+
+	// length := queue.GetLength()
+	// nopkts := 64 * (queue.GetPriority() / sched.quantumSum)
+	nopkts := getNoPacketsToDequeue(-1, queue.GetPriority(), sched.quantumSum)
 	pktToDequeue := min(1, nopkts)
 
-	if length > 0 {
-		qps := queue.PopMultiple(max(length, pktToDequeue))
-		for _, qp := range qps {
-			forwarder(qp.Rp)
+	var qp *qosqueues.QPkt
+
+	for i := 0; i < pktToDequeue; i++ {
+		qp = queue.Pop()
+		if qp == nil {
+			break
 		}
+		forwarder(qp.Rp)
 	}
 }
 
+<<<<<<< a30f4f9ce1fcd6781928bcadc9ab117c3f393840:go/border/qos/scheduler/drrScheduler.go
 func (sched *deficitRoundRobinScheduler) Dequeuer(routerConfig queues.InternalRouterConfig, forwarder func(rp *rpkt.RtrPkt)) {
+=======
+func (sched *DeficitRoundRobinScheduler) Dequeuer(routerConfig qosqueues.InternalRouterConfig, forwarder func(rp *rpkt.RtrPkt)) {
+>>>>>>> UP:go/border/qos/qosscheduler/drrScheduler.go
 	if sched.totalLength == 0 {
 		panic("There are no queues to dequeue from. Please check that Init is called")
 	}
@@ -51,10 +63,10 @@ func (sched *deficitRoundRobinScheduler) Dequeuer(routerConfig queues.InternalRo
 		for i := 0; i < sched.totalLength; i++ {
 			sched.Dequeue(routerConfig.Queues[i], forwarder, i)
 		}
-		time.Sleep(1 * time.Millisecond)
+		time.Sleep(500 * time.Nanosecond)
 	}
 }
 
-func (sched *deficitRoundRobinScheduler) GetMessages() *chan bool {
+func (sched *DeficitRoundRobinScheduler) GetMessages() *chan bool {
 	return &sched.messages
 }
