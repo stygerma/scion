4e0302af9f7e25ad3996f191c79ab5380f270755
Juan A. Garcia Pardo <juagargi@gmail.com>
2020-04-09 16:24:46 +0200

    cleanup
    # Conflicts:
    #	go/border/qos/scheduler/rrScheduler.go

--- go/border/qos/scheduler/drrScheduler.go
+++ go/border/qos/scheduler/drrScheduler.go
@@ -1,3 +1,17 @@
+// Copyright 2020 ETH Zurich
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package scheduler
 
 import (
@@ -6,8 +20,8 @@ import (
 	"github.com/scionproto/scion/go/lib/log"
 )
 
-// This is a deficit round robin dequeuer. Queues with higher priority will have more packets dequeued at the same time.
-
+// deficitRoundRobinScheduler is a deficit round robin dequeuer.
+// Queues with higher priority will have more packets dequeued at the same time.
 type deficitRoundRobinScheduler struct {
 	quantumSum  int
 	totalLength int
@@ -17,7 +31,6 @@ type deficitRoundRobinScheduler struct {
 var _ SchedulerInterface = (*deficitRoundRobinScheduler)(nil)
 
 func (sched *deficitRoundRobinScheduler) Init(routerConfig queues.InternalRouterConfig) {
-
 	sched.quantumSum = 0
 	sched.totalLength = len(routerConfig.Queues)
 
@@ -27,15 +40,15 @@ func (sched *deficitRoundRobinScheduler) Init(routerConfig queues.InternalRouter
 
 }
 
-func (sched *deficitRoundRobinScheduler) dequeue(routerConfig queues.InternalRouterConfig, forwarder func(rp *rpkt.RtrPkt), queueNo int) {
+func (sched *deficitRoundRobinScheduler) dequeue(routerConfig queues.InternalRouterConfig,
+	forwarder func(rp *rpkt.RtrPkt), queueNo int) {
 
 	length := routerConfig.Queues[queueNo].GetLength()
 	var nopkts int = 64 * (routerConfig.Queues[queueNo].GetPriority() / sched.quantumSum)
 	pktToDequeue := min(1, nopkts)
 
-	log.Debug("The queue has length", "length", length)
-	log.Debug("Dequeueing packets", "quantum", pktToDequeue)
-
+	log.Trace("The queue has length", "length", length)
+	log.Trace("Dequeueing packets", "quantum", pktToDequeue)
 	if length > 0 {
 		qps := routerConfig.Queues[queueNo].PopMultiple(max(length, pktToDequeue))
 		for _, qp := range qps {
