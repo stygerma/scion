616a8b8727742579598a721a12b3c1812799eb2b
Juan A. Garcia Pardo <juagargi@gmail.com>
2020-04-09 16:39:32 +0200

    cleanup

--- go/border/rpkt/export.go
+++ go/border/rpkt/export.go
@@ -28,9 +28,26 @@ import (
 	"github.com/scionproto/scion/go/lib/topology"
 )
 
-func prepareRtrPacketDetailedSample(sourceIA addr.IA, destinationIA addr.IA, L4Type common.L4ProtocolType) *RtrPkt {
+func PrepareRtrPacketWithStrings(sourceIA string, destinationIA string, L4Type int) *RtrPkt {
+	srcIA, err := addr.IAFromString(sourceIA)
+	if err != nil {
+		fmt.Println(err)
+	}
+	dstIA, err := addr.IAFromString(destinationIA)
+	if err != nil {
+		fmt.Println(err)
+	}
+	pkt := prepareRtrPacketDetailedSample(
+		srcIA,
+		dstIA,
+		common.L4ProtocolType(L4Type))
+	return pkt
+}
+
+func prepareRtrPacketDetailedSample(sourceIA addr.IA, destinationIA addr.IA,
+	L4Type common.L4ProtocolType) *RtrPkt {
+
 	r := NewRtrPkt()
-	// r.Raw = xtest.MustReadFromFile(t, rawUdpPkt)
 	// Set some other data that are required for the parsing to succeed:
 	var config = &brconf.BRConf{
 		IA: addr.IA{I: 1, A: 2},
@@ -38,13 +55,13 @@ func prepareRtrPacketDetailedSample(sourceIA addr.IA, destinationIA addr.IA, L4T
 			IFs: map[common.IFIDType]*topology.IFInfo{5: nil},
 		},
 	}
+	sampleUDPAddr := &net.UDPAddr{
+		IP:   net.IPv4(127, 1, 1, 111),
+		Port: -1,
+		Zone: "IPv6 scoped addressing zone",
+	}
+
 	r.Ingress = addrIFPair{IfID: 5}
-	// return r
-
-	// r.parseBasic()
-
-	sampleUDPAddr := &net.UDPAddr{IP: net.IPv4(127, 1, 1, 111), Port: -1, Zone: "IPv6 scoped addressing zone"}
-
 	r.Id = "TestPacket000"
 	r.Raw = nil // We don't need contents for this anyways
 	r.TimeIn = time.Now()
@@ -55,47 +72,13 @@ func prepareRtrPacketDetailedSample(sourceIA addr.IA, destinationIA addr.IA, L4T
 	r.Egress = []EgressPair{EgressPair{S: nil, Dst: sampleUDPAddr}}
 	r.CmnHdr = spkt.CmnHdr{NextHdr: common.L4ProtocolType(1)}
 	r.IncrementedPath = false
-	r.idxs = packetIdxs{} // packetIdxs provides offsets into a packet buffer to the start of various, we might actually need Raw
+	r.idxs = packetIdxs{} // packetIdxs provides offsets into a packet buffer
+	// to the start of various, we might actually need Raw
 	r.dstIA = destinationIA
 	r.srcIA = sourceIA
-	r.dstHost = nil
-	r.srcHost = nil
-	r.infoF = nil
-	r.hopF = nil
-	r.ifCurr = nil
-	r.ifNext = nil
-	r.consDirFlag = nil
-	r.HBHExt = nil
-	r.E2EExt = nil
 	r.L4Type = L4Type // L4SCMP
-	r.l4 = nil
-	r.pld = nil
-	r.hooks = hooks{}
-	r.SCMPError = false
-	// r.log.Logger = nil
 	r.Ctx = rctx.New(config)
 	r.refCnt = -1
 
 	return r
-}
-
-func PrepareRtrPacketWithStrings(sourceIA string, destinationIA string, L4Type int) *RtrPkt {
-
-	srcIA, err := addr.IAFromString(sourceIA)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	dstIA, err := addr.IAFromString(destinationIA)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	pkt := prepareRtrPacketDetailedSample(
-		srcIA,
-		dstIA,
-		common.L4ProtocolType(L4Type))
-	return pkt
 }
