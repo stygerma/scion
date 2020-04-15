a807464f40c1ab1d3a9ed82e11826bf6558b7058
Joel Fischer <fischjoe@student.ethz.ch>
2020-04-15 16:08:23 +0200

    cleanup

--- go/border/router.go
+++ go/border/router.go
@@ -140,8 +140,7 @@ func (r *Router) processPacket(rp *rpkt.RtrPkt) {
 		metrics.Process.Pkts(l).Inc()
 		return
 	}
-
-    // Validation looks for errors in the packet that didn't break basic
+	// Validation looks for errors in the packet that didn't break basic
 	// parsing.
 	valid, err := rp.Validate()
 	if err != nil {
@@ -173,8 +172,7 @@ func (r *Router) processPacket(rp *rpkt.RtrPkt) {
 		l.Result = metrics.ErrProcess
 		metrics.Process.Pkts(l).Inc()
 		return
-    }
-
+	}
 	// TODO(joelfischerr): This is for the demo only. Remove this for the final PR.
 	if r.Id == "br1-ff00_0_110-1" {
 		// Enqueue the packet. Packets will be classified, put on different queues,
@@ -187,14 +185,13 @@ func (r *Router) processPacket(rp *rpkt.RtrPkt) {
 
 func (r *Router) forwardPacket(rp *rpkt.RtrPkt) {
 	defer rp.Release()
-
 	// Forward the packet. Packets destined to self are forwarded to the local dispatcher.
 	if err := rp.Route(); err != nil {
 		r.handlePktError(rp, err, "Error routing packet")
-        l := metrics.ProcessLabels{
-            IntfIn:  metrics.IntfToLabel(rp.Ingress.IfID),
-            IntfOut: metrics.Drop,
-        }
+		l := metrics.ProcessLabels{
+			IntfIn:  metrics.IntfToLabel(rp.Ingress.IfID),
+			IntfOut: metrics.Drop,
+		}
 		l.Result = metrics.ErrRoute
 		metrics.Process.Pkts(l).Inc()
 	}
