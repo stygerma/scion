f2c229d9d324cd4990641638645f65f541da5b4e
stygerma <stygerma@student.ethz.ch>
2020-04-20 13:59:14 +0200

    minor changes for demo

--- go/border/bscNotify.go
+++ go/border/bscNotify.go
@@ -10,6 +10,7 @@ import (
 	"github.com/scionproto/scion/go/lib/layers"
 	"github.com/scionproto/scion/go/lib/log"
 	"github.com/scionproto/scion/go/lib/scmp"
+	"github.com/scionproto/scion/go/lib/spath"
 )
 
 const logEnabledBsc = true
@@ -63,9 +64,10 @@ func (r *Router) sendBscNotificationSCMP(qp *queues.QPkt, info *scmp.InfoBscCW) 
 		l4, _ := notification.L4Hdr(false)
 		log.Debug("New SCMP Notification", "SrcIA", srcIA, "SrcHost",
 			srcHost, "DstIA", DstIA, "DstHost", DstHost, "\n RtrAddr", routerAddr,
+			"CurrBW", r.qosConfig.GetQueue(qp.QueueNo).GetTokenBucket().CurrBW,
 			"Pkt ID", id,
 			"\n L4", l4,
-			"\n Congestion Warning", pld) //"CurrBW", r.qosConfig.GetQueue(qp.QueueNo).GetTockenBucket().CurrBW,
+			"\n Congestion Warning", pld) //
 	}
 	notification.Route()
 
@@ -79,70 +81,79 @@ func (r *Router) createBscSCMPNotification(qp *queues.QPkt,
 		srcHost, _ := qp.Rp.SrcHost()
 		DstIA, _ := qp.Rp.DstIA()
 		DstHost, _ := qp.Rp.DstHost()
+		l4hdr := qp.Rp.GetL4Hdr()
 		log.Debug("New queueing packet", "SrcIA", srcIA, "SrcHost",
-			srcHost, "DstIA", DstIA, "DstHost", DstHost, "Pkt ID", qp.Rp.Id)
+			srcHost, "DstIA", DstIA, "DstHost", DstHost, "Pkt ID", qp.Rp.Id,
+			"\n L4Type", qp.Rp.L4Type, "L4Header", l4hdr)
 	}
 	id := qp.Rp.Id
 	sp, err := qp.Rp.CreateReplyScnPkt()
 	if err != nil {
 		return nil, err, ""
 	}
+	// log.Debug("We getting here???????????????????????? 1")
 	sp.HBHExt = make([]common.Extension, 0, common.ExtnMaxHBH+1)
 	/*MS: We classify the congestion warning as not erroneous and don't need the
 	Basic congestion warning to be HBH*/
+	// log.Debug("We getting here???????????????????????? 2")
 	ext := &layers.ExtnSCMP{Error: false, HopByHop: false}
+	// log.Debug("We getting here???????????????????????? 3")
 	sp.HBHExt = append(sp.HBHExt, ext)
+	// log.Debug("We getting here???????????????????????? 4")
 	//TODO (stygerma): Add SPSE with DRKey
 
 	sp.Pld = scmp.NotifyPld(info)
+	// log.Debug("We getting here???????????????????????? 5")
 	sp.L4 = scmp.NewHdr(scmp.ClassType{Class: scmp.C_General, Type: scmp.T_G_BasicCongWarn}, sp.Pld.Len())
 	log.Debug("Created SPkt reply", "sp", sp, "Pkt ID", id)
 	reply, err := qp.Rp.CreateReply(sp)
-	if logEnabledBsc {
-		srcIA, _ := reply.SrcIA()
-		srcHost, _ := reply.SrcHost()
-		DstIA, _ := reply.DstIA()
-		DstHost, _ := reply.DstHost()
-		log.Debug("Created RPkt reply", "SrcIA", srcIA, "SrcHost",
-			srcHost, "DstIA", DstIA, "DstHost", DstHost, "Pkt ID", id)
-	}
+	// log.Debug("We getting here???????????????????????? 6")
+	// log.Debug("What do we have here?", "Rpkt", reply.String())
+	// if logEnabledBsc {
+	// 	srcIA, _ := reply.SrcIA()
+	// 	srcHost, _ := reply.SrcHost()
+	// 	DstIA, _ := reply.DstIA()
+	// 	DstHost, _ := reply.DstHost()
+	// 	log.Debug("Created RPkt reply", "SrcIA", srcIA, "SrcHost",
+	// 		srcHost, "DstIA", DstIA, "DstHost", DstHost, "Pkt ID", id)
+	// }
 	return reply, err, id
 }
 
 func (r *Router) createBscCongWarn(np *queues.NPkt) *scmp.InfoBscCW {
-	// restrictionPrint := r.config.Queues[np.Qpkt.QueueNo].GetCongestionWarning()
-	// testing := r.config.Queues[np.Qpkt.QueueNo].GetMinBandwidth()
-	// restriction := 3
-	// if logEnabledBsc {
-	// 	log.Debug("restrictions on information content", "restriction", restrictionPrint, "MinBW", testing)
-	// }
-	// if restriction > 3 {
-	// 	log.Error("Unable to create congestion warning", "restriction on information content", restriction)
-	// 	return nil
-	// }
-	// bscCW := &scmp.InfoBscCW{}
-	// bscCW.ConsIngress = common.IFIDType(np.Qpkt.Rp.Ingress.IfID)
-	// bscCW.Path = &spath.Path{
-	// 	Raw:    np.Qpkt.Rp.Raw[(np.Qpkt.Rp).GetPathIdx():np.Qpkt.Rp.CmnHdr.HdrLenBytes()],
-	// 	InfOff: np.Qpkt.Rp.CmnHdr.InfoFOffBytes() - (np.Qpkt.Rp).GetPathIdx(),
-	// 	HopOff: np.Qpkt.Rp.CmnHdr.HopFOffBytes() - (np.Qpkt.Rp).GetPathIdx()}
-	// if logEnabledBsc {
-	// 	log.Debug("InfoBscCW", "ConsIngress", common.IFIDType(np.Qpkt.Rp.Ingress.IfID),
-	// 		"QueueLength", (r.config.Queues[np.Qpkt.QueueNo]).GetLength(), "CurrBW",
-	// 		r.config.Queues[np.Qpkt.QueueNo].GetTokenBucket().CurrBW, "QueueFullness",
-	// 		(r.config.Queues[np.Qpkt.QueueNo]).GetFillLevel(), "Violation", np.Qpkt.Act.Reason)
-	// }
-	// if restriction > 0 {
-	// 	bscCW.QueueLength = uint64((r.config.Queues[np.Qpkt.QueueNo]).GetLength())
-	// }
-	// if restriction > 1 {
-	// 	bscCW.CurrBW = r.config.Queues[np.Qpkt.QueueNo].GetTokenBucket().CurrBW
-	// 	bscCW.QueueFullness = uint64((r.config.Queues[np.Qpkt.QueueNo]).GetFillLevel())
-	// }
-	// if restriction > 2 {
-	// 	bscCW.Violation = uint64(np.Qpkt.Act.Reason)
-	// 	//bscCW.ClassRule = np.Qpkt.Act.Rule
-	// }
+	restrictionPrint := r.qosConfig.GetQueue(np.Qpkt.QueueNo).GetCongestionWarning()
+	testing := r.qosConfig.GetQueue(np.Qpkt.QueueNo).GetMinBandwidth()
+	restriction := 3
+	if logEnabledBsc {
+		log.Debug("restrictions on information content", "restriction", restrictionPrint, "MinBW", testing)
+	}
+	if restriction > 3 {
+		log.Error("Unable to create congestion warning", "restriction on information content", restriction)
+		return nil
+	}
 	bscCW := &scmp.InfoBscCW{}
+	bscCW.ConsIngress = common.IFIDType(np.Qpkt.Rp.Ingress.IfID)
+	bscCW.Path = &spath.Path{
+		Raw:    np.Qpkt.Rp.Raw[(np.Qpkt.Rp).GetPathIdx():np.Qpkt.Rp.CmnHdr.HdrLenBytes()],
+		InfOff: np.Qpkt.Rp.CmnHdr.InfoFOffBytes() - (np.Qpkt.Rp).GetPathIdx(),
+		HopOff: np.Qpkt.Rp.CmnHdr.HopFOffBytes() - (np.Qpkt.Rp).GetPathIdx()}
+	if logEnabledBsc {
+		log.Debug("InfoBscCW", "ConsIngress", common.IFIDType(np.Qpkt.Rp.Ingress.IfID),
+			"QueueLength", (r.qosConfig.GetQueue(np.Qpkt.QueueNo)).GetLength(), "CurrBW",
+			r.qosConfig.GetQueue(np.Qpkt.QueueNo).GetTokenBucket().CurrBW, "QueueFullness",
+			r.qosConfig.GetQueue(np.Qpkt.QueueNo).GetFillLevel(), "Violation", np.Qpkt.Act.GetReason())
+	}
+	if restriction > 0 {
+		bscCW.QueueLength = uint64(r.qosConfig.GetQueue(np.Qpkt.QueueNo).GetLength())
+	}
+	if restriction > 1 {
+		bscCW.CurrBW = uint64(r.qosConfig.GetQueue(np.Qpkt.QueueNo).GetTokenBucket().CurrBW)
+		bscCW.QueueFullness = uint64(r.qosConfig.GetQueue(np.Qpkt.QueueNo).GetFillLevel())
+	}
+	if restriction > 2 {
+		bscCW.Violation = uint64(np.Qpkt.Act.GetReason())
+		//bscCW.ClassRule = np.Qpkt.Act.Rule
+	}
+	//bscCW := &scmp.InfoBscCW{}
 	return bscCW
 }
