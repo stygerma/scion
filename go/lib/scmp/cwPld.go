e3e34f28fb99faa0dc7de1c7703024e6db009675
stygerma <stygerma@student.ethz.ch>
2020-04-21 20:30:46 +0200

    added quoted L4 hdr to CWMeta

--- go/lib/scmp/cwPld.go
+++ go/lib/scmp/cwPld.go
@@ -10,8 +10,9 @@ import (
 var _ common.Payload = (*CWPayload)(nil)
 
 type CWPayload struct {
-	Meta *CWMeta
-	Info Info
+	Meta  *CWMeta
+	Info  Info
+	L4Hdr common.RawBytes
 }
 
 func CWPldFromRaw(b common.RawBytes, t Type) (*CWPayload, error) {
@@ -24,15 +25,15 @@ func CWPldFromRaw(b common.RawBytes, t Type) (*CWPayload, error) {
 	if p.Info, err = ParseInfo(buf.Next(int(p.Meta.InfoLen)*common.LineLen), ClassType{Class: C_General, Type: t}); err != nil {
 		return nil, err
 	}
+	p.L4Hdr = buf.Next(int(p.Meta.L4HdrLen) * common.LineLen)
 	return p, nil
 }
 
-/*TODO: MS: implement this similarly to the PldFromQuotes function but instead
-of deciding which headers to quote, restrict the information content*/
-func NotifyPld(info Info) *CWPayload {
+func NotifyPld(info Info, l4 common.L4ProtocolType, f QuoteFunc) *CWPayload {
 	p := &CWPayload{Info: info}
-
-	p.Meta = &CWMeta{}
+	p.L4Hdr = f(RawL4Hdr) //only include the L4Hdr thus don't need classTypeQuotes()
+	p.Meta = &CWMeta{L4HdrLen: uint8(len(p.L4Hdr) / common.LineLen),
+		L4Proto: l4}
 	if info != nil {
 		p.Meta.InfoLen = uint8(p.Info.Len() / common.LineLen)
 	}
@@ -46,6 +47,7 @@ func (p *CWPayload) Copy() (common.Payload, error) {
 	c := &CWPayload{}
 	c.Meta = p.Meta.Copy()
 	c.Info = p.Info
+	c.L4Hdr = append(common.RawBytes(nil), p.L4Hdr...)
 	return c, nil
 }
 
@@ -54,14 +56,19 @@ func (p *CWPayload) WritePld(b common.RawBytes) (int, error) {
 		return 0, common.NewBasicError("Not engough space in buffer", nil,
 			"actual", len(b), "expected", p.Len())
 	}
+	offset := 0
 	if err := p.Meta.Write(b[0:]); err != nil {
 		return 0, err
 	}
+	offset += CWMetaLen
 	if p.Info != nil {
-		if _, err := p.Info.Write(b[CWMetaLen:]); err != nil {
+		if count, err := p.Info.Write(b[offset:]); err != nil {
 			return 0, err
+		} else {
+			offset += count
 		}
 	}
+	copy(b[offset:], p.L4Hdr)
 	return p.Len(), nil
 }
 
@@ -70,6 +77,7 @@ func (p *CWPayload) Len() int {
 	if p.Info != nil {
 		l += p.Info.Len()
 	}
+	l += int(p.Meta.L4HdrLen) * common.LineLen
 	return l
 }
 
@@ -78,6 +86,9 @@ func (p *CWPayload) String() string {
 	fmt.Fprintf(buf, "Meta: %v\n", p.Meta)
 	if p.Info != nil {
 		fmt.Fprintf(buf, "Info: %v\n", p.Info)
+	}
+	if p.L4Hdr != nil {
+		fmt.Fprintf(buf, "L4Hdr: %v\n", p.L4Hdr)
 	}
 	return buf.String()
 }
